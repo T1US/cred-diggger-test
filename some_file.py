95c5c7a048a25d10e44468dfc754acd204cfdee6
T1US <42844387+T1US@users.noreply.github.com>
2022-09-22 11:42:07 +0500

    Create some_file.py

---
+++ some_file.py
@@ -0,0 +1,11 @@
+import requests
+
+key="EAACEdEose0cBAddopsad903291"
+
+some_other_str="Hello my name is T1us, nice to meet you"
+another_cred = AKIA1234567899876543
+
+my_password = "qwerty123$"
+
+def some_func():
+  print("Hello World!")
